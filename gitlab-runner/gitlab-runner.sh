3d0dc68546e65454014b60447e1b4cc2158e68e3
wjh2012 <wjh2012@naver.com>
2024-07-14 15:45:39 +0900

    gitlab-runner

---
+++ gitlab-runner/gitlab-runner.sh
@@ -0,0 +1,6 @@
+docker run -d --name gitlab-runner --restart always \
+  -v /srv/gitlab-runner/config:/etc/gitlab-runner \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  gitlab/gitlab-runner:latest
+
+docker run --rm -it -v /srv/gitlab-runner/config:/etc/gitlab-runner gitlab/gitlab-runner register
