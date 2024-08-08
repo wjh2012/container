ff1a57505cea3f7339f4fe730b6ce9b20ba32e56
wjh <wjh2012@naver.com>
2024-08-08 17:40:49 +0900

    rabbit cluster sh

--- rabbitMQ/cluster.sh
+++ rabbitMQ/cluster.sh
@@ -9,3 +9,5 @@ docker exec -it rabbitmq3 rabbitmqctl stop_app
 docker exec -it rabbitmq3 rabbitmqctl reset
 docker exec -it rabbitmq3 rabbitmqctl join_cluster rabbit@rabbitmq1
 docker exec -it rabbitmq3 rabbitmqctl start_app
+
+echo "RabbitMQ 클러스터 설정이 완료되었습니다."
