903f0f4c61c6e17c179304b4338c28163a93932d
wjh <wjh2012@naver.com>
2024-08-08 17:34:12 +0900

    rabbitMQ

---
+++ rabbitMQ/cluster.sh
@@ -0,0 +1,9 @@
+docker exec -it rabbitmq2 rabbitmqctl stop_app
+docker exec -it rabbitmq2 rabbitmqctl reset
+docker exec -it rabbitmq2 rabbitmqctl join_cluster rabbit@rabbitmq1
+docker exec -it rabbitmq2 rabbitmqctl start_app
+
+docker exec -it rabbitmq3 rabbitmqctl stop_app
+docker exec -it rabbitmq3 rabbitmqctl reset
+docker exec -it rabbitmq3 rabbitmqctl join_cluster rabbit@rabbitmq1
+docker exec -it rabbitmq3 rabbitmqctl start_app
