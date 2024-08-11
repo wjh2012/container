9f2f22456af998aea1fe12528b3874737820f552
wjh2012 <wjh2012@naver.com>
2024-08-11 15:58:10 +0900

    cuda docker port

--- cuda/dev/docker-build.sh
+++ cuda/dev/docker-build.sh
@@ -1,1 +1,1 @@
-docker run -d --name test --gpus all -p 11022:22 -v C:\Users\wjh20\Desktop\prj:/workspace/prj cuda-dev
+docker run -d --name test --gpus all -p 30022:22 -v C:\Users\wjh20\Desktop\prj:/workspace/prj cuda-dev
